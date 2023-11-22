0b7e3848cd06d2303c175ac4a1400627394ec073
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2023-11-22 11:35:03 +0530

    Initial Commit

---
+++ API based Authentication/API_based_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_based_AuthenticationApp.swift
+//  API based Authentication
+//
+//  Created by Thilina Shashimal Senarath on 2023-11-22.
+//
+
+import SwiftUI
+
+@main
+struct API_based_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
